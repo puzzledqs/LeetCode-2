1b7cbece2da9b0cfbde736ede8abbd6281cde102
qchero <327951547@qq.com>
2013-09-03 19:26:06 +0800

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -1,3 +1,55 @@
+// updated 090313
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char> > &matrix) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (matrix.size() == 0)
+            return 0;
+        if (matrix[0].size() == 0)
+            return 0;
+            
+        int r = matrix.size();
+        int c = matrix[0].size();
+        
+        int ans = 0, nowh, last;
+        vector<int> height(c, 0);
+        stack<int> si, sh;
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (matrix[i][j] == '0')    {
+                    height[j] = 0;
+                }
+                else    {
+                    height[j]++;
+                }
+            }
+            
+            for (int j = 0; j <= c; j++)    {
+                nowh = (j == c ? -1 : height[j]);
+                
+                if (si.empty() || sh.top() < nowh)  {
+                    si.push(j);
+                    sh.push(nowh);
+                }
+                else if (sh.top() > nowh)   {
+                    while (!si.empty() && nowh < sh.top()) {
+                        last = si.top();
+                        ans = max(ans, sh.top() * (j - si.top()));
+                        si.pop();
+                        sh.pop();
+                    }
+                    si.push(last);
+                    sh.push(nowh);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
 class Solution {
 public:
     int maximalRectangle(vector<vector<char> > &matrix) {
